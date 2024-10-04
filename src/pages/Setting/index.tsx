7784b84e3609b223b39e997ebdbd588ab170cd3b
lvollmer <lautarovollmer0@gmail.com>
2024-10-04 16:22:28 -0300

    validate role

--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -3,6 +3,8 @@ import AdminCard from "./AdminCard";
 import { getAllAdmins } from "@store/services/admin";
 import DataAdmin from "./DataAdmin";
 import CreateAdmin from "./CreateAdmin";
+import { useSelector } from "react-redux";
+import { RootState } from "@store";
 
 export interface IAdmin {
   id: string;
@@ -15,7 +17,7 @@ export interface IAdmin {
 const Setting = () => {
   const [admins, setAdmins] = useState<IAdmin[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const adminsPerPage = 5; 
+  const adminsPerPage = 5;
 
   const fetchAdmins = async () => {
     const response = await getAllAdmins();
@@ -27,8 +29,10 @@ const Setting = () => {
   }, []);
 
   const indexOfLastAdmin = currentPage * adminsPerPage;
-  const indexOfFirstAdmin = indexOfLastAdmin - adminsPerPage; 
-  const currentAdmins = admins.slice(indexOfFirstAdmin, indexOfLastAdmin); 
+  const indexOfFirstAdmin = indexOfLastAdmin - adminsPerPage;
+  const currentAdmins = admins.slice(indexOfFirstAdmin, indexOfLastAdmin);
+
+  const { user } = useSelector((state: RootState) => state.auth);
 
   // Funciones para manejar la paginación
   const totalPages = Math.ceil(admins.length / adminsPerPage);
@@ -47,56 +51,64 @@ const Setting = () => {
 
   return (
     <>
-      <div className="flex flex-col pl-16 pt-12 px-10 h-[100%] w-[1100px]">
-        <p className="text-[3rem] text-argenpesos-textos font-bold pb-14">
-          Configuración
-        </p>
-        <CreateAdmin fetchAdmins={fetchAdmins} />
+      {user?.role === "admin" ? (
+        <div className="flex flex-col pl-16 pt-12 px-10 h-[100%] w-[1100px]">
 
-        {admins.length !== 0 && (
-          <div className="flex justify-between gap-5 my-8 max-w-[950px]">
-            <p className="text-[1rem] text-argenpesos-textos font-bold">
-              Nombre
-            </p>
-            <p className="text-[1rem] text-argenpesos-textos font-bold">
-              Email
-            </p>
-            <p className="text-[1rem] text-argenpesos-textos font-bold"></p>
-          </div>
-        )}
-
-        {currentAdmins.map(admin => (
-          <AdminCard key={admin.id} admin={admin} fetchAdmins={fetchAdmins} />
-        ))}
-
-        <div className="flex justify-between mt-4">
-          <button
-            onClick={prevPage}
-            disabled={currentPage === 1 || admins.length === 0} // Deshabilitar si no hay admins
-            className={`flex w-[147px] h-[38px] items-center justify-center bg-argenpesos-skyBlue text-[1rem] text-argenpesos-white font-book rounded-[5px] hover:bg-argenpesos-blue hover:transition-colors duration-100 ${
-              currentPage === 1 || admins.length === 0
-                ? "opacity-50 cursor-not-allowed"
-                : ""
-            }`}
-          >
-            Anterior
-          </button>
-          <span className="text-[1rem] text-argenpesos-textos font-bold">
-            Página {currentPage} de {totalPages}
-          </span>
-          <button
-            onClick={nextPage}
-            disabled={currentPage === totalPages}
-            className={`flex w-[147px] h-[38px] items-center justify-center bg-argenpesos-skyBlue text-[1rem] text-argenpesos-white font-book rounded-[5px] hover:bg-argenpesos-blue hover:transition-colors duration-100 ${
-              currentPage === totalPages ? "opacity-50 cursor-not-allowed" : ""
-            }`}
-          >
-            Siguiente
-          </button>
+          <DataAdmin />
         </div>
+      ) : (
+        <div className="flex flex-col pl-16 pt-12 px-10 h-[100%] w-[1100px]">
+          <p className="text-[3rem] text-argenpesos-textos font-bold pb-14">
+            Configuración
+          </p>
+          <CreateAdmin fetchAdmins={fetchAdmins} />
 
-        <DataAdmin />
-      </div>
+          {admins.length !== 0 && (
+            <div className="flex justify-between gap-5 my-8 max-w-[950px]">
+              <p className="text-[1rem] text-argenpesos-textos font-bold">
+                Nombre
+              </p>
+              <p className="text-[1rem] text-argenpesos-textos font-bold">
+                Email
+              </p>
+              <p className="text-[1rem] text-argenpesos-textos font-bold"></p>
+            </div>
+          )}
+
+          {currentAdmins.map(admin => (
+            <AdminCard key={admin.id} admin={admin} fetchAdmins={fetchAdmins} />
+          ))}
+
+          <div className="flex justify-between mt-4">
+            <button
+              onClick={prevPage}
+              disabled={currentPage === 1 || admins.length === 0} // Deshabilitar si no hay admins
+              className={`flex w-[147px] h-[38px] items-center justify-center bg-argenpesos-skyBlue text-[1rem] text-argenpesos-white font-book rounded-[5px] hover:bg-argenpesos-blue hover:transition-colors duration-100 ${
+                currentPage === 1 || admins.length === 0
+                  ? "opacity-50 cursor-not-allowed"
+                  : ""
+              }`}
+            >
+              Anterior
+            </button>
+            <span className="text-[1rem] text-argenpesos-textos font-bold">
+              Página {currentPage} de {totalPages}
+            </span>
+            <button
+              onClick={nextPage}
+              disabled={currentPage === totalPages}
+              className={`flex w-[147px] h-[38px] items-center justify-center bg-argenpesos-skyBlue text-[1rem] text-argenpesos-white font-book rounded-[5px] hover:bg-argenpesos-blue hover:transition-colors duration-100 ${
+                currentPage === totalPages
+                  ? "opacity-50 cursor-not-allowed"
+                  : ""
+              }`}
+            >
+              Siguiente
+            </button>
+          </div>
+          <DataAdmin />
+        </div>
+      )}
     </>
   );
 };
